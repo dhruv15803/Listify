2e0985daacb36de4d20dc72cc2fc3d5f42558633
dhruv15803 <122736571+dhruv15803@users.noreply.github.com>
2024-01-07 23:22:09 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useEffect, useState } from "react";
 import Header from "./Components/Header"
 import PendingTasks from "./Pages/PendingTasks"
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
@@ -6,14 +6,26 @@ import CompletedTasks from "./Pages/CompletedTasks";
 
 function App() {
 
-  const [tasks,setTasks] = useState([
-    {
+  let tasksLocal = JSON.parse(localStorage.getItem('tasksLocal_Listify'));
+  let completedTasksLocal = JSON.parse(localStorage.getItem('completedTasksLocal_Listify'));
+
+  if(tasksLocal===undefined || tasksLocal===null){
+    tasksLocal = [{
       "id":0,
       "task":"",
-    }
-  ]);
-  const [completedTasks,setCompletedTasks] = useState([]);
+    }]
+  }
+  if(completedTasksLocal===undefined || completedTasksLocal===null){
+    completedTasksLocal = [];
+  }
 
+  const [tasks,setTasks] = useState(tasksLocal);
+  const [completedTasks,setCompletedTasks] = useState(completedTasksLocal);
+
+  useEffect(()=>{
+    localStorage.setItem('tasksLocal_Listify',JSON.stringify(tasks));
+    localStorage.setItem('completedTasksLocal_Listify',JSON.stringify(completedTasks));
+  },[tasks,completedTasks])
 
   return (
     <>
